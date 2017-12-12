b7204b8124c08d1340e8cbd8dff98395282d07aa
Felipe Condon Silva <felipe.condon.13@sansano.usm.cl>
2017-12-12 02:02:39 -0300

    Some final changes
    Ready for second deliverable.

--- homeSecurity/platforms/browser/www/js/init.js
+++ homeSecurity/platforms/browser/www/js/init.js
@@ -5,3 +5,13 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+// $('#login-button').click(function(){
+// 	Materialize.toast('')
+// });
+
+$('#password').click(function(){
+	Materialize.toast('Acerque tarjeta de acceso.');
+	setTimeout(function(){
+		$('#password').val('asdadasdasdasdasdasdascascasc');
+	},1000);
+});
